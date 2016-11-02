8a233adef10566846b944c29cc636e22886fbfdb
Alexander <Alexander.mumma@gmail.com>
2016-11-02 21:37:04 +1300

    Initial Commit - Simple GUI Mockup

---
+++ BarcodingApplication/BarcodingApplication/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BarcodingApplication
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.lstLabels = new System.Windows.Forms.ListBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(53, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(537, 286);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 1;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // lstLabels
+            // 
+            this.lstLabels.FormattingEnabled = true;
+            this.lstLabels.Location = new System.Drawing.Point(12, 41);
+            this.lstLabels.Name = "lstLabels";
+            this.lstLabels.Size = new System.Drawing.Size(600, 238);
+            this.lstLabels.TabIndex = 2;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(130, 12);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(53, 23);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(71, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(53, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lstLabels);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnNew);
+            this.Name = "frmMain";
+            this.Text = "Label Program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.ListBox lstLabels;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
